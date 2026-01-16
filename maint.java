b345c8f51ec3f38b316c80b4b5ee187fde97f615
Jeremiah Aderinto <aderintojeremiah257@gmail.com>
2026-01-16 10:20:56 +0100

    Updated Exercises

---
+++ maint.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class maint {
+    public static void main(String[] args) {
+        int richManAmount = 1000000;
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the amount you have:");
+        int userAmount = userInput.nextInt();
+        if (userAmount > richManAmount) {
+            System.out.println("You are richer than the rich man!");
+        } else {
+            System.out.println("You are not richer than the rich man.");
+        }
+    }
+}
