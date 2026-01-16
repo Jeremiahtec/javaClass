20d2ddc4cddfbe9641bef328b2f6dc359392147e
Jeremiah Aderinto <aderintojeremiah257@gmail.com>
2026-01-16 09:56:44 +0100

    First try

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+        public static void main() {
+        Scanner amount = new Scanner(System.in);
+        System.out.println("Enter your the amount you want to deposit:");
+        int depositAmount = amount.nextInt();
+        System.out.println("Enter your the amount you want to deposit:");
+        int depositAmount2 = amount.nextInt();
+        System.out.println("Enter your the amount you want to deposit:");
+        int depositAmount3 = amount.nextInt();
+        int totalAmount = depositAmount + depositAmount2 + depositAmount3;
+        System.out.println("The total amount you have deposited is: " + totalAmount);
+    }
+}
